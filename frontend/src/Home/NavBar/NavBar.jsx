8630c4f44315fdb0966318e6c9808c23bd854e30
sangeetha <122152646+sangssss@users.noreply.github.com>
2024-06-27 20:01:38 +0530

    modified

--- frontend/src/Home/NavBar/NavBar.jsx
+++ frontend/src/Home/NavBar/NavBar.jsx
@@ -1,11 +1,16 @@
-
 import React, { useState } from 'react';
 import './NavBar.css';
-import { RiPlanetLine, RiMenuLine, RiCloseLine, RiArrowDownSLine, RiPieChartLine, RiArrowUpDownLine, RiBarChartLine, RiAddLine, RiFileListLine, RiCashLine, RiRefund2Line, RiUserLine, RiLockLine, RiMessage3Line } from 'react-icons/ri';
+import { RiMenuLine, RiCloseLine, RiArrowDownSLine, RiPieChartLine, RiArrowUpDownLine, RiBarChartLine, RiAddLine, RiFileListLine, RiCashLine, RiRefund2Line, RiUserLine, RiLockLine, RiMessage3Line, RiHome7Fill, RiGitRepositoryPrivateFill, RiBookFill, RiMoneyDollarBoxFill, RiMailAddFill, RiUserShared2Line, RiUserFollowFill, RiUserSmileFill, RiUserReceived2Fill, RiUserShared2Fill, RiUserFill, RiSchoolFill, RiChatPrivateFill, RiProjector2Fill, RiHomeOfficeFill, RiBookMarkedFill, RiBookOpenFill } from 'react-icons/ri';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [dropdownOpen, setDropdownOpen] = useState({ analytics: false, reports: false, users: false });
+  const [dropdownOpen, setDropdownOpen] = useState({
+    institute: false,
+    administration: false,
+    academics: false,
+    centers: false,
+    connect: false
+  });
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -17,43 +22,82 @@ const Navbar = () => {
 
   return (
     <header className="header">
-        <img src='/images/chennaiLogo.png' style={{width:"400px",height:"80px"}}></img>
+      <img src='/images/chennaiLogo.png' style={{ width: "400px", height: "80px" }}></img>
       <nav className="header__nav">
         <div className="anchors">
           <a href="#">Home</a>
           <a href="#">Company</a>
 
-          <div className="dropdown__item" onClick={() => toggleDropdown('analytics')}>
+          <div className={`dropdown__item ${dropdownOpen.institute ? 'active' : ''}`} onClick={() => toggleDropdown('institute')}>
             <div className="nav__link">
-              Analytics <RiArrowDownSLine className={`dropdown__arrow ${dropdownOpen.analytics ? 'rotate' : ''}`} />
+              Institute <RiArrowDownSLine className={`dropdown__arrow ${dropdownOpen.institute ? 'rotate' : ''}`} />
             </div>
-            <ul className={`dropdown__menu ${dropdownOpen.analytics ? 'show' : ''}`}>
-              <li><a href="#" className="dropdown__link"><RiPieChartLine /> Overview</a></li>
-              <li><a href="#" className="dropdown__link"><RiArrowUpDownLine /> Transactions</a></li>
-              <li className="dropdown__subitem" onClick={() => toggleDropdown('reports')}>
-                <div className="dropdown__link">
-                  <RiBarChartLine /> Reports <RiAddLine className={`dropdown__add ${dropdownOpen.reports ? 'rotate' : ''}`} />
-                </div>
-                <ul className={`dropdown__submenu ${dropdownOpen.reports ? 'show' : ''}`}>
-                  <li><a href="#" className="dropdown__sublink"><RiFileListLine /> Documents</a></li>
-                  <li><a href="#" className="dropdown__sublink"><RiCashLine /> Payments</a></li>
-                  <li><a href="#" className="dropdown__sublink"><RiRefund2Line /> Refunds</a></li>
-                </ul>
-              </li>
+            <ul className={`dropdown__menu ${dropdownOpen.institute ? 'show' : ''}`}>
+              <li><a href="#"><RiHome7Fill /> Homepage</a></li>
+              <li><a href="#"><RiCashLine /> About Us</a></li>
+              <li><a href="#"><RiGitRepositoryPrivateFill /> Board of Governs</a></li>
+              <li><a href="#"><RiBookFill /> Academic Council</a></li>
+              <li><a href="#"><RiCashLine /> Memorandum of Association</a></li>
+              <li><a href="#"><RiMoneyDollarBoxFill /> Annual Reports</a></li>
+              <li><a href="#"><RiMailAddFill /> News Letter</a></li>
             </ul>
           </div>
-          <a href="#">Products</a>
-          <div className="dropdown__item" onClick={() => toggleDropdown('users')}>
+
+          <div className={`dropdown__item ${dropdownOpen.administration ? 'active' : ''}`} onClick={() => toggleDropdown('administration')}>
             <div className="nav__link">
-              Users <RiArrowDownSLine className={`dropdown__arrow ${dropdownOpen.users ? 'rotate' : ''}`} />
+              Administration <RiArrowDownSLine className={`dropdown__arrow ${dropdownOpen.administration ? 'rotate' : ''}`} />
             </div>
-            <ul className={`dropdown__menu ${dropdownOpen.users ? 'show' : ''}`}>
-              <li><a href="#" className="dropdown__link"><RiUserLine /> Profiles</a></li>
-              <li><a href="#" className="dropdown__link"><RiLockLine /> Accounts</a></li>
-              <li><a href="#" className="dropdown__link"><RiMessage3Line /> Messages</a></li>
+            <ul className={`dropdown__menu ${dropdownOpen.administration ? 'show' : ''}`}>
+              <li><a href="#"><RiUserShared2Fill /> Director</a></li>
+              <li><a href="#"><RiUserFollowFill /> Chairman</a></li>
+              <li><a href="#"><RiUserSmileFill /> Deans</a></li>
+              <li><a href="#"><RiUserReceived2Fill/> Department Heads</a></li>
+              <li><a href="#"><RiAddLine /> Committees & Incharges</a></li>
+              <li><a href="#"><RiFileListLine /> Past Directors & Chairpersons</a></li>
+              <li><a href="#"><RiCashLine /> Administration Dept.</a></li>
+              <li><a href="#"><RiRefund2Line /> Annual Project Report</a></li>
             </ul>
           </div>
-          <a href="#">Contact</a>
+
+          <div className={`dropdown__item ${dropdownOpen.academics ? 'active' : ''}`} onClick={() => toggleDropdown('academics')}>
+            <div className="nav__link">
+              Academics <RiArrowDownSLine className={`dropdown__arrow ${dropdownOpen.academics ? 'rotate' : ''}`} />
+            </div>
+            <ul className={`dropdown__menu ${dropdownOpen.academics ? 'show' : ''}`}>
+              <li><a href="#"><RiPieChartLine /> Overview</a></li>
+              <li><a href="#"><RiArrowUpDownLine /> Study @ NITTTR</a></li>
+              <li><a href="#"><RiUserFill /> Faculty & Departments</a></li>
+              <li><a href="#"><RiSchoolFill /> Learning for All</a></li>
+              <li><a href="#"><RiChatPrivateFill /> Professional Development Program</a></li>
+              <li><a href="#"><RiFileListLine /> International Training Program</a></li>
+              <li><a href="#"><RiMessage3Line /> SWAYAM NITTT Modules</a></li>
+            </ul>
+          </div>
+
+          <div className={`dropdown__item ${dropdownOpen.centers ? 'active' : ''}`} onClick={() => toggleDropdown('centers')}>
+            <div className="nav__link">
+              Centers & Facilities <RiArrowDownSLine className={`dropdown__arrow ${dropdownOpen.centers ? 'rotate' : ''}`} />
+            </div>
+            <ul className={`dropdown__menu ${dropdownOpen.centers ? 'show' : ''}`}>
+              <li><a href="#"><RiBookOpenFill /> Academics, Studies & Research</a></li>
+              <li><a href="#"><RiArrowUpDownLine /> International Affairs</a></li>
+              <li><a href="#"><RiHomeOfficeFill /> Resource Centre</a></li>
+            </ul>
+          </div>
+
+          <div className={`dropdown__item ${dropdownOpen.connect ? 'active' : ''}`} onClick={() => toggleDropdown('connect')}>
+            <div className="nav__link">
+              Connect <RiArrowDownSLine className={`dropdown__arrow ${dropdownOpen.connect ? 'rotate' : ''}`} />
+            </div>
+            <ul className={`dropdown__menu ${dropdownOpen.connect ? 'show' : ''}`}>
+              <li><a href="#"><RiUserFill/> Alumni</a></li>
+              <li><a href="#"><RiUserLine /> Students</a></li>
+              <li><a href="#"><RiProjector2Fill /> Projects & Awards</a></li>
+            </ul>
+          </div>
+
+          <a href="#">Contact Us</a>
+
         </div>
 
         <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
